56c336bb38cf75b22ae5223566a76eb700c07322
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-07-16 19:43:14 +0900

    Accept multi-line input

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ pub mod expr;
 extern crate combine;
 extern crate combine_language;
 
+use std::io::prelude::Read;
 use combine::Parser;
 
 fn main() {
   let mut input = String::new();
-  std::io::stdin().read_line(&mut input).unwrap();
+  std::io::stdin().read_to_string(&mut input).unwrap();
   let input: &str = &input;
   let judge = evalml3::judgement_parser().easy_parse(input).unwrap().0;
   let proof = evalml3::prove(judge.env, judge.expr);
