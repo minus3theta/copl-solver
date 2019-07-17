5ab48b99500d191d9f16da720d5c2258c08b56ae
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-07-17 16:43:10 +0900

    Implement cons parser/prover

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@
 pub mod evalml1;
 pub mod evalml2;
 pub mod evalml3;
+pub mod evalml4;
 pub mod expr;
 #[macro_use]
 extern crate combine;
 extern crate combine_language;
 
-use std::io::prelude::Read;
 use combine::Parser;
+use std::io::prelude::Read;
 
 fn main() {
   let mut input = String::new();
   std::io::stdin().read_to_string(&mut input).unwrap();
   let input: &str = &input;
-  let judge = evalml3::judgement_parser().easy_parse(input).unwrap().0;
-  let proof = evalml3::prove(judge.env, judge.expr);
+  let judge = evalml4::judgement_parser().easy_parse(input).unwrap().0;
+  let proof = evalml4::prove(judge.env, judge.expr);
   println!("{}", proof);
 }
